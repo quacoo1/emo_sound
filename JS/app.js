9d4f743a65d520e1359148563a8bf740ca3eae1f
Duality <quacoo.boat@gmail.com>
2020-04-19 23:55:01 +0000

    Emotion bar

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,28 @@
-const app = new Vue({
 
+const app = new Vue({
+    el: '#app',
+    data:{
+        moods:[
+            {
+                id: 1231,
+                name: 'happy',
+                icon: '😊'
+            },
+            {
+                id: 1232,
+                name: 'sad',
+                icon: '😞'
+            },
+            {
+                id: 1233,
+                name: 'angry',
+                icon: '😠'
+            },
+            {
+                id: 1234,
+                name: 'in-the-spirt',
+                icon: '😇'
+            }
+        ]
+    }
 })
